52506ee0676a68709572bf8816172be4c04b54d2
mukulk6 <mynameismukul@gmail.com>
2024-01-20 21:09:18 +0530

    new changes

--- app/config/index.tsx
+++ app/config/index.tsx
@@ -1,12 +1,43 @@
 export const PRODUCT_CATEGORIES = [
     {
-        label:"UI Kits",
-        value:"ui_kits" as const,
-        featured:[
+        label: "UI Kits",
+        value: "ui_kits" as const,
+        featured: [
             {
-            name:"Editor picks",
-            href:"#",
-            iamgeSrc:"/nav/u-kits/mixed.jpg"
+                name: "Editor picks",
+                href: "#",
+                iamgeSrc: "/nav/ui-kits/mixed.jpg"
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                iamgeSrc: "/nav/ui-kits/blue.jpg"
+            },
+            {
+                name: "Editor picks",
+                href: "#",
+                iamgeSrc: "/nav/ui-kits/purple.jpg"
+            }
+        ]
+    },
+    {
+        label: "Icons",
+        value: "icons" as const,
+        featured: [
+            {
+                name: "Favourite Icons Picks",
+                href: "#",
+                iamgeSrc: "/nav/icons/picks.jpg"
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                iamgeSrc: "/nav/ui-kits/new.jpg"
+            },
+            {
+                name: "Best selling icons",
+                href: "#",
+                iamgeSrc: "/nav/ui-kits/bestsellers.jpg"
             }
         ]
     }
--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,9 +1,35 @@
 "use client"
-import { useState } from "react"
+import { PRODUCT_CATEGORIES } from "@/app/config"
+import { useState } from "react";
+import NavItem from "./NavItem";
 const NavItems = () => {
     const [activeIndex,setActiveIndex] = useState<null | Number>(null)
+
+    const isAnyOpen = activeIndex !== null
     return(
-        <div className="flex gap-4 h-full"></div>
+        <div className="flex gap-4 h-full">
+            {
+                PRODUCT_CATEGORIES.map((category,index)=>{
+                    const handleOpen=() =>{
+                        if(activeIndex === index )
+                        {
+                            setActiveIndex(null)
+                        }
+                        else
+                        {
+                            setActiveIndex(index)
+                        }
+                    }
+
+                    const isOpen = index === activeIndex
+
+                    
+                    return(
+                        <NavItem category={category} isAnyOpen={isAnyOpen} handleOpen={handleOpen} isOpen={isOpen} key={category.value} />
+                    )
+                })
+            }
+        </div>
     )
 }
 
