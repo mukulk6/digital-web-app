23fdde06df8ee3f7a853fa06e2bb05c9c691ea9a
mukulk6 <mynameismukul@gmail.com>
2024-02-10 19:13:44 +0530

    changes

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,8 +1,14 @@
 "use client"
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { Separator } from "@radix-ui/react-separator";
 import { ShoppingCartIcon } from "lucide-react";
+import { formatPrice } from "../lib/utils";
+import { buttonVariants } from "./ui/button";
+import Link from "next/link";
 
 const Cart = () => {
+    const itemCount = 0;
+    const fee = 1;
     return(
      <Sheet>
         <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -15,6 +21,48 @@ const Cart = () => {
             <SheetHeader className="space-y-2.5 pr-6">
                 <SheetTitle>Cart(0)</SheetTitle>
             </SheetHeader>
+            {itemCount > 0 ? (
+                <>
+                <div className="w-full flex pr-6 flex-col">
+                    {/* Cart logic */}
+                    cart items
+                </div>
+                <div className="space-y-4 pr-6">
+                   <Separator />
+                   <div className="space-y-1.5 pr-6">
+                    <div className="flex">
+                        <span className="flex-1">
+                            Shipping
+                        </span>
+                        <span>Free</span>
+                    </div>
+                    <div className="flex">
+                        <span className="flex-1">
+                            Transaction Fee
+                        </span>
+                        <span>{formatPrice(fee)}</span>
+                    </div>
+                    <div className="flex">
+                        <span className="flex-1">
+                            Total
+                        </span>
+                        <span>{formatPrice(fee)}</span>
+                    </div>
+                   </div>
+                   <SheetFooter>
+                    <SheetTrigger asChild>
+                        <Link href='/cart' className={buttonVariants({className:"w-full"})}>
+                            Continue to checkout
+                        </Link>
+                    </SheetTrigger>
+                   </SheetFooter>
+                </div>
+                </>
+            ):(<div className="flex h-full flex-col items-center justify-center space-y-1">
+                <div className="relative mb-4 h-60 w-60 text">
+                
+                </div>
+            </div>)}
         </SheetContent>   
      </Sheet>   
     )
