c703e97df0d912756d0122a257edf5808764b090
mukulk6 <mynameismukul@gmail.com>
2024-03-23 14:24:25 +0530

    changes

---
+++ src/trpc/auth-router.ts
@@ -0,0 +1,27 @@
+import { TRPCError } from "@trpc/server"
+import { getPayloadClient } from "../get-payload"
+import { AuthCrediantialsValidator } from "../lib/validators/account-credentials-validators"
+import {router,publicProcedure} from "./trpc"
+export const authRouter = router({
+    createPayloadUser:publicProcedure.input(AuthCrediantialsValidator).mutation(async ({input})=>{
+        const {email,password} = input
+        const payload = await getPayloadClient();
+
+        const {docs:users} = await payload.find({
+            collection:"users",
+            where:{
+                email:{
+                    equals:email
+                }
+            }
+        })
+        if(users.length !== 0) throw new TRPCError({code:'CONFLICT'})
+
+        await payload.create({
+            collection:"users",
+            data:{
+                
+            }
+        })
+    })
+})
