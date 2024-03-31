b0a465f75e2b5723896ad9dc63f6d2823dd9a800
mukulk6 <mynameismukul@gmail.com>
2024-04-01 00:31:17 +0530

    changes

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -2,10 +2,20 @@ import dotenv from "dotenv";
 import path from "path";
 import type { InitOptions } from "payload/config";
 import payload, { Payload } from "payload"
+import nodemailer from "nodemailer"
 dotenv.config({
     path:path.resolve(__dirname,"../.env")
 })
 
+const transporter = nodemailer.createTransport({
+    host:"smtp.resend.com",
+    secure:true,
+    port:465,
+    auth:{
+        user:"resend",
+        pass:process.env.RESEND_API_KEY
+    }
+})
 let cached = (global as any).payload
 
 if(!cached)
@@ -31,6 +41,9 @@ export const getPayloadClient = async({initOptions} : Args = {}):Promise<Payload
     if(!cached.promise)
     {
         cached.promise = payload.init({
+            email:{transport:transporter,
+            fromAddress:"onbarding@resend.com",
+        fromName:'digitalHippo'},
             secret:process.env.PAYLOAD_SECRET,
             local:initOptions?.express ? false : true,
             ...(initOptions || {})
